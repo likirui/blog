1309c6cea3d3b67c3931df7e1fc17ba21bd30e99
smartlivo <smartlivingstone95@gmail.com>
2019-05-18 11:18:57 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+function footer(){
+    var d=new Date();
+document.getElementById("the_footer").innerHTML =
+"<p>&copy;  " + new Date(d).getFullYear() + " Webstar. All rights reserved.<p>";
+    }
---
+++ schjs.js
@@ -0,0 +1,5 @@
+function footer(){
+    var d=new Date();
+document.getElementById("the_footer").innerHTML =
+"<p>&copy;  " + new Date(d).getFullYear() + " Nandihills Academy. All rights reserved.<p>";
+    }
